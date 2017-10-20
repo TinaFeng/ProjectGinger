d276a2cf3b4e6c2d64f476c7fae2870233bfb6e3
TinaFeng <xuanfeng1996@gmail.com>
2017-10-20 16:48:35 -0700

    File Organization and Player Controller
    Players can move around and has basic colliders up. 2 Players can also
    pass through each other

---
+++ Sweet Escape/Assets/Scripts/Player_Controller.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Controller : MonoBehaviour {
+
+    public float speed = 5f;
+
+    float current_speed_x, current_speed_y;
+
+    BoxCollider2D self;
+	// Use this for initialization
+	void Start () {
+        
+        self = this.GetComponent<BoxCollider2D>();
+	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+            Physics2D.IgnoreCollision(collision.collider, self);
+    }
+
+    // Update is called once per frame
+    void Update () {
+        current_speed_x = Input.GetAxis("Horizontal") * speed;
+        current_speed_y = Input.GetAxis("Vertical") * speed;
+
+	}
+
+    private void FixedUpdate()
+    {
+
+        this.transform.Translate(Time.deltaTime * current_speed_x * Vector2.right);
+        this.transform.Translate(Time.deltaTime * current_speed_y * Vector2.up);
+    }
+}
---
+++ Sweet Escape/Assets/Scripts/Player_Controller_P2.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Controller_P2 : MonoBehaviour {
+
+    public float speed = 5f;
+
+
+    public float current_speed_x, current_speed_y;
+
+    BoxCollider2D self;
+    // Use this for initialization
+    void Start()
+    {
+
+        self = this.GetComponent<BoxCollider2D>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+            Physics2D.IgnoreCollision(collision.collider, self);
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        current_speed_x = Input.GetAxis("Horizontal2") * speed;
+        current_speed_y = Input.GetAxis("Vertical2") * speed;
+
+    }
+
+    private void FixedUpdate()
+    {
+
+        this.transform.Translate(Time.deltaTime * current_speed_x * Vector2.right);
+        this.transform.Translate(Time.deltaTime * current_speed_y * Vector2.up);
+    }
+}
